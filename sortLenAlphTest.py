f0bcb52f9363a8080edb62e429597102ece7880c
Ryan <ryan_ursus@outlook.com>
2021-03-31 13:48:32 -0400

    Updated test to look for required files in the same directory

--- sortLenAlphTest.py
+++ sortLenAlphTest.py
@@ -12,34 +12,32 @@ class sortedTesting(unittest.TestCase):
     # Test if text was sorted in ascending order correctly
     def testAscending(self):
         # Need to first make sure the files with which we test with exist
-        if os.path.isfile('ascendingTest.txt') and os.path.isfile("../Sort Me.txt"):
+        if os.path.isfile('ascendingTest.txt') and os.path.isfile("Sort Me.txt"):
             # Get the sorted text file as an array to compare with
             sortedAscending = sortLenAlph.getWords('ascendingTest.txt')
 
             # Use to sort function
-            ascendingTest = sortLenAlph.sortText("../Sort Me.txt", True)
+            ascendingTest = sortLenAlph.sortText("Sort Me.txt", True)
 
             self.assertEqual(ascendingTest, sortedAscending, "Sorting in Ascending order: FAIL")
         else:
             self.fail('Error: testing files not found. To run tests, the files (ascendingTest.txt) and '
-                      '(descendingTest.txt) must be in the same "Test" folder as sortLenAlphTest.py and (Sort Me.txt) '
-                      'must be in the main folder')
+                      '(descendingTest.txt) must be in the same folder as sortLenAlphTest.py along with (Sort Me.txt)')
 
     # Test if text was sorted in descending order correctly
     def testDescending(self):
         # Need to first make sure the files with which we test with exist
-        if os.path.isfile('descendingTest.txt') and os.path.isfile("../Sort Me.txt"):
+        if os.path.isfile('descendingTest.txt') and os.path.isfile("Sort Me.txt"):
             # Get the sorted text file as an array to compare with
             sortedDescending = sortLenAlph.getWords('descendingTest.txt')
 
             # Use to sort function
-            descendingTest = sortLenAlph.sortText("../Sort Me.txt", False)
+            descendingTest = sortLenAlph.sortText("Sort Me.txt", False)
 
             self.assertEqual(descendingTest, sortedDescending, "Sorting in Descending order: FAIL")
         else:
             self.fail('Error: testing files not found. To run tests, the files (ascendingTest.txt) and '
-                      '(descendingTest.txt) must be in the same "Test" folder as sortLenAlphTest.py and (Sort Me.txt) '
-                      'must be in the main folder')
+                      '(descendingTest.txt) must be in the same folder as sortLenAlphTest.py along with (Sort Me.txt)')
 
 
 
